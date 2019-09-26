06a6082bf0333ed6295afafb628362e57f3fa253
Ben <benjisgarza@gmail.com>
2019-09-26 10:59:01 -0700

    Added Comment model and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_many :posts
+    has_many :comments
     before_save { self.email = email.downcase }
     validates :name, presence: true, length: { maximum: 30 },
                      uniqueness: { case_sensitive: false }
