1fca08fa54b8b44530bfcab10cc221d13e727cbd
Ben <benjisgarza@gmail.com>
2019-09-26 12:45:18 -0700

    Linter fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
   has_many :comments
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 30 },
-                     uniqueness: { case_sensitive: false }
+                   uniqueness: { case_sensitive: false }
   validates :email, presence: true, length: { maximum: 255 },
-                     uniqueness: { case_sensitive: false }
+                    uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
 end
